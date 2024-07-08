8de309bd17d36d5a94200c55064c1bc4709fa13f
flaringapp <flaringapp@gmail.com>
2024-07-08 12:55:04 +0300

    Add collapsing top bar snap scope documentation

--- ComposeCollapsingTopBar/src/main/java/com/flaringapp/compose/topbar/snap/CollapsingTopBarSnapScope.kt
+++ ComposeCollapsingTopBar/src/main/java/com/flaringapp/compose/topbar/snap/CollapsingTopBarSnapScope.kt
@@ -18,8 +18,21 @@ package com.flaringapp.compose.topbar.snap
 
 import com.flaringapp.compose.topbar.CollapsingTopBarControls
 
+/**
+ * The receiver scope of [CollapsingTopBarSnapBehavior]. Creates a scoped snapping environment of
+ * something that represents top bar state as [CollapsingTopBarControls], which even may be
+ * dynamic. Responsible for executing snap action requested in [snapWithProgress].
+ */
 fun interface CollapsingTopBarSnapScope {
 
+    /**
+     * Executes snap action [action] providing current progress with [CollapsingTopBarControls]
+     * receiver.
+     *
+     * @param wasMovingUp the last motion direction that resulted in snap being requested. May
+     * help complex snap scopes decide where to snap, i.e. what [CollapsingTopBarControls] to use.
+     * @param action the snap action lambda to be executed.
+     */
     suspend fun snapWithProgress(
         wasMovingUp: Boolean,
         action: suspend CollapsingTopBarControls.(progress: Float) -> Unit,
