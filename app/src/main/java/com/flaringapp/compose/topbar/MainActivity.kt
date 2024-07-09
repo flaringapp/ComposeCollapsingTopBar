a6f4686d00f8bb269d4a2e87e16fe49241a131e5
flaringapp <flaringapp@gmail.com>
2024-07-09 14:53:26 +0300

    Add parallax snap collapsing sample

--- app/src/main/java/com/flaringapp/compose/topbar/MainActivity.kt
+++ app/src/main/java/com/flaringapp/compose/topbar/MainActivity.kt
@@ -20,7 +20,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.flaringapp.compose.topbar.ui.samples.SimpleCollapsingSample
+import com.flaringapp.compose.topbar.ui.samples.ParallaxSnapCollapsingSample
 import com.flaringapp.compose.topbar.ui.theme.ComposeCollapsingTopBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCollapsingTopBarTheme {
-                SimpleCollapsingSample(
+                ParallaxSnapCollapsingSample(
                     onBack = {},
                 )
             }
