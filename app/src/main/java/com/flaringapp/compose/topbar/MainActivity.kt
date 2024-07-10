3252babcc543f73b227c65497bf64663926b78ef
flaringapp <flaringapp@gmail.com>
2024-07-10 21:02:01 +0300

    Add column moving element sample

--- app/src/main/java/com/flaringapp/compose/topbar/MainActivity.kt
+++ app/src/main/java/com/flaringapp/compose/topbar/MainActivity.kt
@@ -51,6 +51,7 @@ import com.flaringapp.compose.topbar.ui.samples.basic.CollapsingExpandAlwaysSamp
 import com.flaringapp.compose.topbar.ui.samples.basic.CollapsingExpandAtTopSample
 import com.flaringapp.compose.topbar.ui.samples.basic.EnterAlwaysCollapsedSample
 import com.flaringapp.compose.topbar.ui.samples.column.AlternatelyCollapsibleColumnSample
+import com.flaringapp.compose.topbar.ui.samples.column.ColumnMovingElementSample
 import com.flaringapp.compose.topbar.ui.samples.column.ColumnInStackSample
 import com.flaringapp.compose.topbar.ui.samples.column.FullyCollapsibleColumnSample
 import com.flaringapp.compose.topbar.ui.samples.column.PartiallyCollapsibleColumnSample
@@ -132,6 +133,7 @@ private fun rememberSampleGroups(): SnapshotStateList<SamplesGalleryGroup> {
                     PartiallyCollapsibleColumnSample,
                     AlternatelyCollapsibleColumnSample,
                     ColumnInStackSample,
+                    ColumnMovingElementSample,
                 ),
             ),
             SamplesGalleryGroup(
