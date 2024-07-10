c5f1bded2ceb1d2fa1ba8bd45e990cc40a123d69
flaringapp <flaringapp@gmail.com>
2024-07-10 23:16:00 +0300

    Prevent vertical fade composable from occupying vertical space

--- app/src/main/java/com/flaringapp/compose/topbar/ui/samples/common/SampleVerticalFadingEdge.kt
+++ app/src/main/java/com/flaringapp/compose/topbar/ui/samples/common/SampleVerticalFadingEdge.kt
@@ -26,8 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.flaringapp.compose.topbar.ui.theme.ComposeCollapsingTopBarTheme
 
@@ -45,6 +47,12 @@ fun SampleVerticalFadingEdge(
 
     Box(
         modifier = modifier
+            .layout { measurable, constraints ->
+                val placeable = measurable.measure(constraints)
+                layout(placeable.width, 0) {
+                    placeable.place(IntOffset.Zero)
+                }
+            }
             .fillMaxWidth()
             .height(height)
             .background(brush),
@@ -55,6 +63,8 @@ fun SampleVerticalFadingEdge(
 @Composable
 private fun Preview() {
     ComposeCollapsingTopBarTheme {
-        SampleVerticalFadingEdge()
+        Box(modifier = Modifier.height(40.dp)) {
+            SampleVerticalFadingEdge()
+        }
     }
 }
