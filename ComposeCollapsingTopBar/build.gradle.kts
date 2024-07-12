51a4b5b7ffab1a8f206105f49913e74a46a62368
flaringapp <flaringapp@gmail.com>
2024-07-12 14:05:18 +0300

    Add android publishing variant

--- ComposeCollapsingTopBar/build.gradle.kts
+++ ComposeCollapsingTopBar/build.gradle.kts
@@ -38,6 +38,13 @@ android {
     composeCompiler {
         reportsDestination = layout.buildDirectory.dir("compose_reports")
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
