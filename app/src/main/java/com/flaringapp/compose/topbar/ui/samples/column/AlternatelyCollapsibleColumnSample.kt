f69c60cbc1c94c5548d649dc23de5f095bf98d4e
flaringapp <flaringapp@gmail.com>
2024-10-21 15:53:46 +0300

    Add positive z index for fixed footer in AlternatelyCollapsibleColumnSample

--- app/src/main/java/com/flaringapp/compose/topbar/ui/samples/column/AlternatelyCollapsibleColumnSample.kt
+++ app/src/main/java/com/flaringapp/compose/topbar/ui/samples/column/AlternatelyCollapsibleColumnSample.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.flaringapp.compose.topbar.nestedcollapse.CollapsingTopBarColumn
 import com.flaringapp.compose.topbar.scaffold.CollapsingTopBarScaffold
 import com.flaringapp.compose.topbar.scaffold.CollapsingTopBarScaffoldScrollMode
@@ -103,7 +104,9 @@ private fun CollapsingContent(
                 SampleFilterChips()
 
                 FixedElement(
-                    modifier = Modifier.notCollapsible(),
+                    modifier = Modifier
+                        .notCollapsible()
+                        .zIndex(1f),
                     text = "Fixed top bar footer",
                 )
             }
