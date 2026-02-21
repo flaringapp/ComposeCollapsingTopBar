41f9cd7b2ba4517550a47dff2ea2e0a21c2ae4b2
Flaringapp <flaringapp@gmail.com>
2026-02-21 13:52:26 +0200

    Update min sdk to 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.flaringapp.compose.topbar"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
