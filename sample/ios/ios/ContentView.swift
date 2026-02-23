31e1f6f2b4305a750d008e6c08328a0588821b29
Flaringapp <flaringapp@gmail.com>
2026-02-23 21:56:40 +0200

    Add empty ios sample project

---
+++ sample/ios/ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios
+//
+//  Created by Andrii Shpek on 23.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample/ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Andrii Shpek on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
