447d6c0d7a4c201635aa53acfe0999ae6c6db1c7
Flaringapp <flaringapp@gmail.com>
2026-02-23 21:22:20 +0200

    Remove redundant dependencies

--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -55,17 +55,10 @@ composeCompiler {
 }
 
 dependencies {
-    implementation(project(":ComposeCollapsingTopBar"))
     implementation(project(":sample:shared"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.materialIconsCore)
-    debugImplementation(libs.androidx.compose.ui.tooling)
 
     lintChecks(libs.slack.compose.linter)
 }
