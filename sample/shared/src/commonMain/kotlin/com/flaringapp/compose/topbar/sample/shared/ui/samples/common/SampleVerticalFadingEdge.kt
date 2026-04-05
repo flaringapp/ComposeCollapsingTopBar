c49c232c52bbb2e9d6ba553f8807cf8b33555813
Flaringapp <flaringapp@gmail.com>
2026-04-05 11:17:46 +0300

    Cleanup sample vertical fading edge

--- sample/shared/src/commonMain/kotlin/com/flaringapp/compose/topbar/sample/shared/ui/samples/common/SampleVerticalFadingEdge.kt
+++ sample/shared/src/commonMain/kotlin/com/flaringapp/compose/topbar/sample/shared/ui/samples/common/SampleVerticalFadingEdge.kt
@@ -20,16 +20,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.flaringapp.compose.topbar.sample.shared.ui.theme.ComposeCollapsingTopBarTheme
 
@@ -47,13 +47,9 @@ fun SampleVerticalFadingEdge(
 
     Box(
         modifier = modifier
-            .layout { measurable, constraints ->
-                val placeable = measurable.measure(constraints)
-                layout(placeable.width, 0) {
-                    placeable.place(IntOffset.Zero)
-                }
-            }
             .fillMaxWidth()
+            .height(0.dp)
+            .wrapContentHeight(Alignment.Top, unbounded = true)
             .height(height)
             .background(brush),
     )
